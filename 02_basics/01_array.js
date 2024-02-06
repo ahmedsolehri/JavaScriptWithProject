5c68f8205ff35e231db6d6e875e140316d01c6a7
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2024-02-06 15:39:36 +0000

    array in js

---
+++ 02_basics/01_array.js
@@ -0,0 +1,26 @@
+const myArray = [1,2,3,4,4,5]
+const myHer = ["Hello", "suparman", "spideman"] 
+
+
+const myArray2 = new Array(12,3,3,5,7)
+// console.log(myArray2[3]);
+
+// myArray.push(100)
+// console.log(myArray);
+
+// myArray.pop()
+// myArray.unshift(101)
+myArray.shift()
+console.log(myArray);
+myArray.shift()
+console.log(myArray);
+
+
+
+// array shallow copy bnata hai
+// shallow mein jo ap change karein gein wo change ho jaye gaoriginal array mein
+// deep copy mein changes nahi hoti orignal array mein
+
+
+
+
