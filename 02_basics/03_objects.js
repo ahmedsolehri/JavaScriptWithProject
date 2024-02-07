eabc3ad1f25bd7c35bad959c842c063bef5489db
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2024-02-07 11:26:48 +0000

    objects in js

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,13 @@
+const jsUser = {
+    name: "Ahmed",
+    "full name": "Ahmed Farooq",
+    age: 20,
+    location: "Lahore",
+    email: "ahmed@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday", "Sunday"]
+}
+
+console.log(jsUser.name)
+console.log(jsUser["name"]);
+console.log(jsUser["full name"]);
