0dd77ccaa403c6b83f78271978e072d06f172499
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2024-02-07 12:50:52 +0000

    objects in js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,6 +1,9 @@
+const mySymbl = Symbol("mysymbol")
+
 const jsUser = {
     name: "Ahmed",
     "full name": "Ahmed Farooq",
+    [mySymbl]: "my Symbol",
     age: 20,
     location: "Lahore",
     email: "ahmed@gmail.com",
@@ -8,6 +11,26 @@ const jsUser = {
     lastLoginDays: ["monday", "Sunday"]
 }
 
-console.log(jsUser.name)
-console.log(jsUser["name"]);
-console.log(jsUser["full name"]);
+// console.log(jsUser.name)
+// console.log(jsUser["name"]);
+// console.log(jsUser["full name"]);
+
+jsUser.email = "solehri@gmail.com"
+// console.log(jsUser["email"]);
+
+// Object.freeze(jsUser)
+jsUser.email = "nasir@gmail.com"
+// console.log(jsUser); 
+
+
+jsUser.greetings = function(){
+    console.log("Hello nodeJS developer");
+}
+
+jsUser.greetingsTwo = function(){
+    console.log(`Hello nodeJS developer ${this.isLoggedIn}`);
+}
+
+
+console.log(jsUser.greetings());
+console.log(jsUser.greetingsTwo());
