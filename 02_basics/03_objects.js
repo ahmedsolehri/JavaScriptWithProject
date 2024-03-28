e4680c9ad32b03de679a9fab617efbe27c6ab4a0
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2024-03-28 07:57:47 +0000

    objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,3 +1,6 @@
+// singeton ==> construdtor sy agar bany ga to singleton hoga
+
+
 const mySymbl = Symbol("mysymbol")
 
 const jsUser = {
@@ -24,13 +27,37 @@ jsUser.email = "nasir@gmail.com"
 
 
 jsUser.greetings = function(){
-    console.log("Hello nodeJS developer");
+    // console.log("Hello nodeJS developer");
 }
 
 jsUser.greetingsTwo = function(){
-    console.log(`Hello nodeJS developer ${this.isLoggedIn}`);
+    // console.log(`Hello nodeJS developer ${this.isLoggedIn}`);
 }
 
 
-console.log(jsUser.greetings());
-console.log(jsUser.greetingsTwo());
+// console.log(jsUser.greetings());
+// console.log(jsUser.greetingsTwo());
+
+
+
+// my code for practice
+
+// Object.create ==> constructor method ==>> singleton
+
+const mySymbol = Symbol("key1") 
+
+const User1 = {
+    name: "Hitesh",
+    country: "Pakistan",
+    //how symbbol define in objects
+    [mySymbol]: "key1",
+    isLogged: false,
+    lastLogged: ["monday", "Sunday"]
+}
+
+// console.log(User1.name)
+// console.log(User1.country)
+// console.log(User1['name'])
+// console.log(User1[mySymbol])
+// console.log(User1)
+
