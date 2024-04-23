7e27eb8acef886637a2225cf1dad63bdc85eb9ac
ahmedsolehri <46812884+ahmedsolehri@users.noreply.github.com>
2024-04-23 13:17:27 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,4 +1,6 @@
 // Immediately invoked function expression
+// iife ic liey use hota hai tak global scope sy polluttion na plute ho
+// global scope sy jo poulltion hoti hai usky iife use hota hai, jo bhi variables etc
 
 ( function oneChai(name) {
     console.log(`your name is ${name}`);
